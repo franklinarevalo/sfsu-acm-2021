4bc58aaf7f9fa24dd3e0d0a52ce4fd909476d95a
hi-artem <aakatev@virtru.com>
2021-01-10 19:24:00 -0800

    Push before ceckout

---
+++ src/Components/Team.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Team extends Component {
+  render() {
+
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = 'images/portfolio/' + projects.image;
+        
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <img alt={projects.title} className="profile-pic" src={projectImage} />
+            <div>
+              <a href={projects.url} title={projects.title}>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        )
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Meet Our Team</h1>
+            <div  id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {projects}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Team;
