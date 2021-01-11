b7e152826eba8b4970e27a35905d0578d7f345e0
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2021-01-10 19:39:45 -0800

    more posts

--- src/logged_out/dummy_data/blogPosts.js
+++ src/logged_out/dummy_data/blogPosts.js
@@ -1,18 +1,19 @@
 import React, { Fragment } from "react";
 import { Typography } from "@material-ui/core";
-import blogPost1 from "./images/blogPost1.jpg";
-import blogPost2 from "./images/blogPost2.jpg";
-import blogPost3 from "./images/blogPost3.jpg";
-import blogPost4 from "./images/blogPost4.jpg";
-import blogPost5 from "./images/blogPost5.jpg";
-import blogPost6 from "./images/blogPost6.jpg";
+
 import interviewPrep from "./images/events/interviewPrep.png";
 import adventOfCode from "./images/events/AdventOfCode.png";
 import mockInterview from "./images/events/mockInterview.jpg";
 import iotWorkshop from "./images/events/IoTworkshopflyer.jpeg";
 import arvrWorkshop from "./images/events/arvr.png";
-
 import appWorkshop from "./images/events/appDev.png";
+import githubWorshop from "./images/events/github.jpg";
+import gameSpeaker from "./images/events/speakerGame.png";
+import thinkBeyond from "./images/events/thinkBeyondCode.jpg";
+import resumeWorkshop from "./images/events/resumeWorkshop.png";
+import webDevWorkshop from "./images/events/webWorkshop.png";
+import invent from "./images/events/invent.jpg";
+import designAlgorithm from "./images/events/designAlgorithms.jpg";
 
 const content = (
   <Fragment>
@@ -104,7 +105,7 @@ export default [
     title: "Github Workshop",
     id: 4,
     date: 1572281600,
-    imageSrc: blogPost4,
+    imageSrc: githubWorshop,
     snippet:
       "Be a team champion with your master Git skills through Github",
     content: content
@@ -113,7 +114,7 @@ export default [
     title: "Web Development Workshop",
     id: 5,
     date: 1573281600,
-    imageSrc: blogPost5,
+    imageSrc: webDevWorkshop,
     snippet:
       "Introduction to core tools and concepts of web development",
     content: content
@@ -141,6 +142,51 @@ export default [
     id: 6,
     date: 1575281600,
     imageSrc: iotWorkshop,
+    snippet:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    content: content
+  },
+  {
+    title: "ACM Distinguished Speaker",
+    id: 6,
+    date: 1575281600,
+    imageSrc: gameSpeaker,
+    snippet:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    content: content
+  },
+  {
+    title: "ACM Distinguished Speaker",
+    id: 6,
+    date: 1575281600,
+    imageSrc: thinkBeyond,
+    snippet:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    content: content
+  },
+  {
+    title: "ACM Distinguished Speaker",
+    id: 6,
+    date: 1575281600,
+    imageSrc: invent,
+    snippet:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    content: content
+  },
+  {
+    title: "ACM Distinguished Speaker",
+    id: 6,
+    date: 1575281600,
+    imageSrc: designAlgorithm,
+    snippet:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    content: content
+  },
+  {
+    title: "Online Portfolio Workshop",
+    id: 6,
+    date: 1575281600,
+    imageSrc: resumeWorkshop,
     snippet:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
     content: content
